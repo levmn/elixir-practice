56301b0aca041f01daf75a056a9aa154275d6fd8
levmn <levimncontato@gmail.com>
2022-07-18 14:40:40 -0300

    setup exmon api and authentication

--- web/ex_mon/lib/ex_mon.ex
+++ web/ex_mon/lib/ex_mon.ex
@@ -1,9 +1,16 @@
 defmodule ExMon do
-  @moduledoc """
-  ExMon keeps the contexts that define your domain
-  and business logic.
+  alias ExMon.{Pokemon, Trainer}
+  alias Trainer.Pokemon, as: TrainerPokemon
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defdelegate create_trainer(params), to: Trainer.Create, as: :call
+  defdelegate delete_trainer(params), to: Trainer.Delete, as: :call
+  defdelegate fetch_trainer(params), to: Trainer.Get, as: :call
+  defdelegate update_trainer(params), to: Trainer.Update, as: :call
+
+  defdelegate fetch_pokemon(params), to: Pokemon.Get, as: :call
+
+  defdelegate create_trainer_pokemon(arams), to: TrainerPokemon.Create, as: :call
+  defdelegate delete_trainer_pokemon(arams), to: TrainerPokemon.Delete, as: :call
+  defdelegate fetch_trainer_pokemon(params), to: TrainerPokemon.Get, as: :call
+  defdelegate update_trainer_pokemon(params), to: TrainerPokemon.Update, as: :call
 end
--- web/ex_mon/mix.exs
+++ web/ex_mon/mix.exs
@@ -44,7 +44,10 @@ defmodule ExMon.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:argon2_elixir, "~> 2.0"},
+      {:tesla, "~> 1.4"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
